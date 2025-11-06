26698f05d236c4381177fef6fef581aab9d1064a
User <rimasdraz@gmail.com>
2025-11-06 13:49:14 +0200

    rimasSSSGUnuyiuykjjojon

--- app/src/main/java/com/example/rimasbiy/HomeScreen.java
+++ app/src/main/java/com/example/rimasbiy/HomeScreen.java
@@ -1,8 +1,14 @@
 package com.example.rimasbiy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -12,6 +18,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HomeScreen extends AppCompatActivity {
+    private SearchView searchV;
+    private Button btnR7;
+    private Button btnR8;
+    private Button btnR9;
+    private Button btnR10;
+    private Button btnR11;
+    private Button btnR12;
+    private ImageView imageV5;
+    private ImageView imageV6;
+    private ImageView imageV7;
+    private ImageView imageV8;
+    private ImageView imageV9;
+    private ImageView imageV10;
+    private TextView tv4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +43,62 @@ public class HomeScreen extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        searchV = findViewById(R.id.searchV);
+        btnR7 = findViewById(R.id.btvR7);
+        btnR8 = findViewById(R.id.btnR8);
+        btnR9 = findViewById(R.id.btnR9);
+        btnR10 = findViewById(R.id.btnR10);
+        btnR11 = findViewById(R.id.btnR11);
+        btnR12 = findViewById(R.id.btnR12);
+        imageV5 = findViewById(R.id.imageV5);
+        imageV6 = findViewById(R.id.imageV6);
+        imageV7 = findViewById(R.id.imageV7);
+        imageV8 = findViewById(R.id.imageV8);
+        imageV9 = findViewById(R.id.imageV9);
+        imageV10 = findViewById(R.id.imageV10);
+        tv4 = findViewById(R.id.tv4);
+        btnR7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(HomeScreen.this, IngredientsList.class);
+                startActivity(i);
+            }
+        });
+        btnR8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(HomeScreen.this, IngredientsList.class);
+                startActivity(i);
+            }
+        });
+        btnR9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(HomeScreen.this, IngredientsList.class);
+                startActivity(i);
+            }
+        });
+        btnR10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(HomeScreen.this, IngredientsList.class);
+                startActivity(i);
+            }
+        });
+        btnR11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(HomeScreen.this, IngredientsList.class);
+                startActivity(i);
+            }
+        });
+        btnR12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(HomeScreen.this, IngredientsList.class);
+                startActivity(i);
+            }
         });
     }
     public boolean onCreateOptionsMenu(Menu menu)
