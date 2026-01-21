24c3fdb38fd1f57193ecfdb06b092b57ed472007
User <rimasdraz@gmail.com>
2026-01-21 08:53:59 +0200

    rijjjgjffdlewJH

---
+++ app/src/main/java/com/example/rimasbiy/userTable/ResponseCallback.java
@@ -0,0 +1,23 @@
+package com.example.rimasbiy.userTable;
+
+/**
+ واجهة تحوي دوال لمعالجة وجود رد من جميني او الرد عند حدوث خطا
+ */
+
+public interface ResponseCallback {
+
+        /**
+         * معالجة جواب الطلب من Gemini
+         * @param response جواب الطلب
+         */
+        public void onResponse(String response);
+
+        /**
+         * الرد بحالة وجود خطا
+         * @param error
+         */
+        public void onError(Throwable error);
+
+
+
+}
